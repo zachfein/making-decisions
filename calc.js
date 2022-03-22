6875497a3bcd409884976a6ab00ca8f6bb904a63
zachfein <zvf0307@gmail.com>
2022-03-22 16:45:01 -0600

    calc-complete

--- calc.js
+++ calc.js
@@ -31,12 +31,23 @@ reader.question("What would you like to calculate?", function(input){
     } else if(mathSymbol === "/") {
         let sum = num1 / num2;
         console.log("The answer is ", sum);
-    } else if(mathSymbol = "root") {
+    } else if(mathSymbol === "root") {
         let sum = Math.sqrt(num1);
-        console.log(sum);
-    } else if(mathSymbol = "square") {
-
+        console.log("The answer is ", sum);
+    } else if(mathSymbol === "square") {
+        let sum = Math.pow(num1);
+        console.log("The answer is ", num1 * num1);
+    } else if(mathSymbol === "cube") {
+        let sum = Math.pow(num1, 3);
+        console.log("The Answer is " , sum)
+    } else if(mathSymbol === "pwr") {
+        let sum = num1 ** num2;
+        console.log("The answer is ", sum);
+    } else if(mathSymbol === "rmndr") {
+        let sum = (num1 % num2);
+        console.log("The answer is ", sum)
     }
+        
     
 
 
